f265a8681eb8dd6a385a7d347d1e488f539cc5b6
seoyoung-park <se0y0ung.park@navercorp.com>
2022-12-26 18:40:51 +0900

    wip: save data into collector

--- metric-module/metric/src/main/java/com/navercorp/pinpoint/metric/collector/dao/pinot/PinotExceptionTraceDao.java
+++ metric-module/metric/src/main/java/com/navercorp/pinpoint/metric/collector/dao/pinot/PinotExceptionTraceDao.java
@@ -2,9 +2,15 @@ package com.navercorp.pinpoint.metric.collector.dao.pinot;
 
 import com.navercorp.pinpoint.common.server.bo.exception.SpanEventExceptionBo;
 import com.navercorp.pinpoint.metric.collector.dao.ExceptionTraceDao;
+import com.navercorp.pinpoint.metric.common.model.SpanEventException;
+import com.navercorp.pinpoint.metric.common.model.StringPrecondition;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Repository;
+
+import java.util.Objects;
 
 /**
  * @author intr3p1d
@@ -13,7 +19,14 @@ import org.springframework.stereotype.Repository;
 public class PinotExceptionTraceDao implements ExceptionTraceDao {
     private final Logger logger = LogManager.getLogger(this.getClass());
 
-    public PinotExceptionTraceDao() {
+    private final KafkaTemplate<String, SpanEventException> kafkaSpanEventExceptionTemplate;
+
+    private final String topic;
+
+    public PinotExceptionTraceDao(KafkaTemplate<String, SpanEventException> kafkaSpanEventExceptionTemplate,
+                                  @Value("${kafka.exceptiontrace.topic}") String topic) {
+        this.kafkaSpanEventExceptionTemplate = Objects.requireNonNull(kafkaSpanEventExceptionTemplate, "kafkaSpanEventExceptionTemplate");
+        this.topic = StringPrecondition.requireHasLength(topic, "topic");
     }
 
     @Override
---
+++ metric-module/metric/src/main/java/com/navercorp/pinpoint/metric/common/model/SpanEventException.java
@@ -0,0 +1,7 @@
+package com.navercorp.pinpoint.metric.common.model;
+
+/**
+ * @author intr3p1d
+ */
+public class SpanEventException {
+}
