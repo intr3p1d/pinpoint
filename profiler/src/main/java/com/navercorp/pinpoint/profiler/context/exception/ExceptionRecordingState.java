6a55a138f8f00b45464482e78eb002c88604b7b7
Seoyoung Park <se0y0ung.park@navercorp.com>
2023-03-15 17:16:03 +0900

    [#]Record SpanEventException in SpanRecorder.recordException()

---
+++ profiler/src/main/java/com/navercorp/pinpoint/profiler/context/exception/ExceptionRecordingState.java
@@ -0,0 +1,116 @@
+/*
+ * Copyright 2023 NAVER Corp.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.navercorp.pinpoint.profiler.context.exception;
+
+/**
+ * @author intr3p1d
+ */
+public enum ExceptionRecordingState {
+    CLEAN {
+        @Override
+        public SpanEventException apply(
+                ExceptionRecordingContext exceptionRecordingContext,
+                Throwable current,
+                long currentStartTime,
+                ExceptionIdGenerator idGenerator) {
+            // do nothing
+            return null;
+        }
+    },
+    STARTED {
+        @Override
+        public SpanEventException apply(
+                ExceptionRecordingContext exceptionRecordingContext,
+                Throwable current,
+                long currentStartTime,
+                ExceptionIdGenerator idGenerator) {
+            exceptionRecordingContext.setPrevious(current);
+            exceptionRecordingContext.setStartTime(currentStartTime);
+            return null;
+        }
+    },
+    STACKING {
+        @Override
+        public SpanEventException apply(
+                ExceptionRecordingContext exceptionRecordingContext,
+                Throwable current,
+                long currentStartTime,
+                ExceptionIdGenerator idGenerator) {
+            SpanEventException spanEventException = null;
+            if (!isContinued(exceptionRecordingContext.getPrevious(), current)) {
+                spanEventException = newSpanEventException(
+                        exceptionRecordingContext, idGenerator.nextExceptionId()
+                );
+            }
+            exceptionRecordingContext.setPrevious(current);
+            exceptionRecordingContext.setStartTime(currentStartTime);
+            return spanEventException;
+        }
+    },
+    FLUSH {
+        @Override
+        public SpanEventException apply(
+                ExceptionRecordingContext exceptionRecordingContext,
+                Throwable current,
+                long currentStartTime,
+                ExceptionIdGenerator idGenerator) {
+            SpanEventException spanEventException = newSpanEventException(
+                    exceptionRecordingContext, idGenerator.nextExceptionId()
+            );
+            exceptionRecordingContext.setPrevious(current);
+            exceptionRecordingContext.resetStartTime();
+            return spanEventException;
+        }
+    };
+
+    public static ExceptionRecordingState stateOf(Object previous, Object current) {
+        if (previous == null) {
+            if (current == null) {
+                return CLEAN;
+            }
+            return STARTED;
+        } else {
+            if (current == null) {
+                return FLUSH;
+            }
+            return STACKING;
+        }
+    }
+
+    public static SpanEventException newSpanEventException(ExceptionRecordingContext context, long exceptionId) {
+        return SpanEventException.newSpanEventException(
+                context.getPrevious(), context.getStartTime(), exceptionId
+        );
+    }
+
+    public static boolean isContinued(Throwable previous, Throwable current) {
+        Throwable throwable = current;
+        while (throwable.getCause() != null) {
+            if (throwable == previous) {
+                return true;
+            }
+            throwable = throwable.getCause();
+        }
+        return false;
+    }
+
+    public abstract SpanEventException apply(
+            ExceptionRecordingContext exceptionRecordingContext,
+            Throwable current,
+            long currentStartTime,
+            ExceptionIdGenerator idGenerator
+    );
+}
