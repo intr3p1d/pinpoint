706643055b8553a2949e4ca7860bc8cfb4d3c602
Seoyoung Park <se0y0ung.park@navercorp.com>
2023-04-13 17:17:38 +0900

    [agent] handle npe

--- profiler/src/main/java/com/navercorp/pinpoint/profiler/context/exception/ExceptionWrapper.java
+++ profiler/src/main/java/com/navercorp/pinpoint/profiler/context/exception/ExceptionWrapper.java
@@ -19,19 +19,21 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
+import java.util.Optional;
 
 /**
  * @author intr3p1d
  */
 public class ExceptionWrapper {
+    private static final String EMPTY_STRING = "";
     private final String exceptionClassName;
     private final String exceptionMessage;
     private final StackTraceElementWrapper[] stackTraceElements;
 
-    private ExceptionWrapper(Throwable throwable){
+    private ExceptionWrapper(Throwable throwable) {
         Objects.requireNonNull(throwable);
-        this.exceptionClassName = throwable.getClass().getSimpleName();
-        this.exceptionMessage = throwable.getMessage();
+        this.exceptionClassName = Optional.ofNullable(throwable.getClass().getSimpleName()).orElse(EMPTY_STRING);
+        this.exceptionMessage = Optional.ofNullable(throwable.getMessage()).orElse(EMPTY_STRING);
         this.stackTraceElements = StackTraceElementWrapper.valueOf(throwable.getStackTrace());
     }
 
