9c1c66a6334c14f4e8bbd02fce126a04c6c88b3b
seoyoung-park <se0y0ung.park@navercorp.com>
2026-02-09 16:22:57 +0900

    remove search-application api

--- web/src/main/java/com/navercorp/pinpoint/web/authorization/controller/AgentListController.java
+++ web/src/main/java/com/navercorp/pinpoint/web/authorization/controller/AgentListController.java
@@ -64,188 +64,14 @@ import java.util.Optional;
 @Validated
 public class AgentListController implements AccessDeniedExceptionHandler {
     private final AgentInfoService agentInfoService;
-    private final ServiceTypeRegistryService registry;
-    private final ApplicationFactory applicationFactory;
-    private final ResponseTimeHistogramService responseTimeHistogramService;
-
-    private final ApplicationAgentInfoMapServiceImpl applicationAgentInfoService;
-    private final HyperLinkFactory hyperLinkFactory;
     private final RangeValidator rangeValidator;
-    private final SortByAgentInfo.Rules DEFAULT_SORT_BY = SortByAgentInfo.Rules.AGENT_ID_ASC;
 
     public AgentListController(
             AgentInfoService agentInfoService,
-            ServiceTypeRegistryService registry,
-            ApplicationFactory applicationFactory,
-            ResponseTimeHistogramService responseTimeHistogramService,
-            ApplicationAgentInfoMapServiceImpl applicationAgentInfoService,
-            HyperLinkFactory hyperLinkFactory,
             ConfigProperties configProperties) {
         this.agentInfoService = Objects.requireNonNull(agentInfoService, "agentInfoService");
-        this.registry = Objects.requireNonNull(registry, "registry");
-        this.applicationFactory = Objects.requireNonNull(applicationFactory, "applicationFactory");
-        this.responseTimeHistogramService =
-                Objects.requireNonNull(responseTimeHistogramService, "responseTimeHistogramService");
-        this.applicationAgentInfoService = applicationAgentInfoService;
-        this.hyperLinkFactory = Objects.requireNonNull(hyperLinkFactory, "hyperLinkFactory");
         Objects.requireNonNull(configProperties, "configProperties");
         this.rangeValidator = new ForwardRangeValidator(Duration.ofDays(configProperties.getInspectorPeriodMax()));
-    }
-
-    private static <T> TreeView<InstancesList<T>> treeView(AgentsMapByApplication<T> agentsListsList) {
-        final List<InstancesList<T>> list = agentsListsList.getAgentsListsList();
-        return new StaticTreeView<>(list);
-    }
-
-    @Deprecated
-    @PreAuthorize("hasPermission(#applicationName, 'application', 'inspector')")
-    @GetMapping(value = "/search-application", params = {"application"})
-    public TreeView<InstancesList<AgentStatusAndLink>> getAgentsList(
-            @RequestParam("application") @NotBlank String applicationName,
-            @RequestParam(value = "serviceTypeCode", required = false) Short serviceTypeCode,
-            @RequestParam(value = "serviceTypeName", required = false) String serviceTypeName,
-            @RequestParam(value = "sortBy") Optional<SortByAgentInfo.Rules> sortBy,
-            @RequestParam(value = "query", required = false) String query) {
-        final SortByAgentInfo.Rules paramSortBy = sortBy.orElse(DEFAULT_SORT_BY);
-        final ApplicationAgentListQueryRule applicationAgentListQueryRule = ApplicationAgentListQueryRule.getByValue(query, ApplicationAgentListQueryRule.ALL);
-        final long timestamp = System.currentTimeMillis();
-        final Application application = createApplication(applicationName, serviceTypeCode, serviceTypeName);
-        Range between = Range.between(timestamp, timestamp);
-        TimeWindow timeWindow = new TimeWindow(between);
-        final AgentsMapByHost list = this.applicationAgentInfoService.getAgentsListByApplicationName(
-                application,
-                timeWindow,
-                paramSortBy,
-                applicationAgentListQueryRule,
-                AgentInfoFilters.acceptAll()
-        );
-        return treeView(list);
-    }
-
-    @Deprecated
-    @PreAuthorize("hasPermission(#applicationName, 'application', 'inspector')")
-    @GetMapping(value = "/search-application", params = {"application", "from", "to"})
-    public TreeView<InstancesList<AgentStatusAndLink>> getAgentsList(
-            @RequestParam("application") @NotBlank String applicationName,
-            @RequestParam(value = "serviceTypeCode", required = false) Short serviceTypeCode,
-            @RequestParam(value = "serviceTypeName", required = false) String serviceTypeName,
-            @RequestParam("from") @PositiveOrZero long from,
-            @RequestParam("to") @PositiveOrZero long to,
-            @RequestParam(value = "sortBy") Optional<SortByAgentInfo.Rules> sortBy,
-            @RequestParam(value = "query", required = false) String query) {
-        final SortByAgentInfo.Rules paramSortBy = sortBy.orElse(DEFAULT_SORT_BY);
-        final ApplicationAgentListQueryRule applicationAgentListQueryRule = ApplicationAgentListQueryRule.getByValue(query, ApplicationAgentListQueryRule.ACTIVE_STATUS);
-        final Application application = createApplication(applicationName, serviceTypeCode, serviceTypeName);
-        Range range = Range.between(from, to);
-        rangeValidator.validate(range);
-        TimeWindow timeWindow = new TimeWindow(range);
-
-        final AgentsMapByHost list = this.applicationAgentInfoService.getAgentsListByApplicationName(
-                application,
-                timeWindow,
-                paramSortBy,
-                applicationAgentListQueryRule,
-                AgentInfoFilters.acceptAll()
-        );
-        return treeView(list);
-    }
-
-    //use only for server map list
-    @Deprecated
-    @PreAuthorize("hasPermission(#applicationName, 'application', 'inspector')")
-    @GetMapping(value = "/search-application", params = {"application", "from", "to", "applicationPairs"})
-    public TreeView<InstancesList<AgentStatusAndLink>> getAgentsListWithVirtualNodes(
-            @RequestParam("application") @NotBlank String applicationName,
-            @RequestParam("serviceTypeCode") Short serviceTypeCode,
-            @RequestParam(value = "serviceTypeName", required = false) String serviceTypeName,
-            @RequestParam("from") @PositiveOrZero long from,
-            @RequestParam("to") @PositiveOrZero long to,
-            @RequestParam(value = "sortBy") Optional<SortByAgentInfo.Rules> sortBy,
-            @RequestParam(value = "query", required = false) String query,
-            @RequestParam(value = "applicationPairs", required = false) ApplicationPairs applicationPairs
-    ) {
-        ServiceType serviceType = registry.findServiceType(serviceTypeCode);
-        if (serviceType.isWas()) {
-            final ApplicationAgentListQueryRule serverMapAgentListQueryRule = ApplicationAgentListQueryRule.getByValue(query, ApplicationAgentListQueryRule.ACTIVE_STATISTICS);
-            return getAgentsList(
-                    applicationName, serviceTypeCode, serviceTypeName, from, to, sortBy, String.valueOf(serverMapAgentListQueryRule)
-            );
-        }
-        Range range = Range.between(from, to);
-        rangeValidator.validate(range);
-        TimeWindow timeWindow = new TimeWindow(range);
-
-        final Application application = applicationFactory.createApplication(applicationName, serviceType.getCode());
-
-        final List<Application> fromApplications =
-                pairsToList(applicationPairs.getFromApplications());
-        final List<Application> toApplications =
-                pairsToList(applicationPairs.getToApplications());
-        final ResponseTimeHistogramServiceOption option = new ResponseTimeHistogramServiceOption
-                .Builder(application, timeWindow,
-                fromApplications, toApplications)
-                .build();
-
-        final NodeHistogramSummary nodeHistogramSummary = responseTimeHistogramService.selectNodeHistogramData(
-                option
-        );
-
-        final AgentsMapByHost list = extractVirtualNode(nodeHistogramSummary);
-        return treeView(list);
-    }
-
-    private List<Application> pairsToList(List<ApplicationPair> applicationPairs) {
-        if (CollectionUtils.isEmpty(applicationPairs)) {
-            return Collections.emptyList();
-        }
-        final List<Application> applications = new ArrayList<>(applicationPairs.size());
-        for (ApplicationPair applicationPair : applicationPairs) {
-            final String applicationName = applicationPair.getApplicationName();
-            final short serviceTypeCode = applicationPair.getServiceTypeCode();
-            final Application application = this.applicationFactory.createApplication(applicationName, serviceTypeCode);
-            applications.add(application);
-        }
-        return applications;
-    }
-
-    private AgentsMapByHost extractVirtualNode(NodeHistogramSummary nodeHistogramSummary) {
-        List<InstancesList<AgentStatusAndLink>> listMap = new ArrayList<>();
-        List<ServerGroup> groups = nodeHistogramSummary.getServerGroupList().getServerGroupList();
-        for (ServerGroup group : groups) {
-            List<AgentStatusAndLink> agents = new ArrayList<>();
-            for (ServerInstance instance : group.getInstanceList()) {
-                AgentInfo agentInfo = new AgentInfo();
-                agentInfo.setAgentId(instance.getName());
-                agentInfo.setAgentName(instance.getAgentName(), instance.getName());
-                agentInfo.setHostName(instance.getHostName());
-                agentInfo.setIp(instance.getIp());
-                agentInfo.setServiceType(instance.getServiceType());
-                AgentStatus agentStatus = new AgentStatus(instance.getName(), instance.getStatus(), 0);
-
-                AgentStatusAndLink agentStatusAndLik = new AgentStatusAndLink(
-                        agentInfo,
-                        agentStatus,
-                        newHyperLink(hyperLinkFactory, instance)
-                );
-                agents.add(agentStatusAndLik);
-            }
-            InstancesList<AgentStatusAndLink> instancesList = new InstancesList<>(group.getHostName(), agents);
-
-            listMap.add(instancesList);
-        }
-        return new AgentsMapByHost(new InstancesListMap<>(listMap));
-    }
-
-    private List<HyperLink> newHyperLink(HyperLinkFactory hyperLinkFactory, ServerInstance serverInstance) {
-        if (serverInstance == null) {
-            return List.of();
-        }
-        return hyperLinkFactory.build(LinkSources.from(serverInstance.getHostName(), serverInstance.getIp(), serverInstance.getServiceType()));
-    }
-
-    private static TreeView<InstancesList<AgentStatusAndLink>> treeView(AgentsMapByHost agentsMapByHost) {
-        final List<InstancesList<AgentStatusAndLink>> list = agentsMapByHost.getAgentsListsList();
-        return new StaticTreeView<>(list);
     }
 
     @PreAuthorize("hasPermission(null, null, T(com.navercorp.pinpoint.web.security.PermissionChecker).PERMISSION_ADMINISTRATION_CALL_API_FOR_APP_AGENT_MANAGEMENT)")
@@ -270,18 +96,6 @@ public class AgentListController implements AccessDeniedExceptionHandler {
                         AgentStatusFilters.acceptAll(),
                         range
                 );
-    }
-
-    private Application createApplication(String applicationName, Short serviceTypeCode, String serviceTypeName) {
-        if (StringUtils.hasLength(applicationName)) {
-            if (serviceTypeCode != null) {
-                return applicationFactory.createApplication(applicationName, serviceTypeCode);
-            } else if (serviceTypeName != null) {
-                return applicationFactory.createApplicationByTypeName(applicationName, serviceTypeName);
-            }
-        }
-        // return application with without service type
-        return new Application(applicationName, ServiceType.UNDEFINED);
     }
 
 }
